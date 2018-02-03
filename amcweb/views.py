5eb0479675b46e5fa96e699b155b489636bbecdc
Bindra <jskrnbindra@gmail.com>
2018-02-03 13:35:02 +0530

    bugfix: removed unnecessary get_context_data

--- amcweb/views.py
+++ amcweb/views.py
@@ -10,10 +10,6 @@ from .models import Appointment, Patient, Prescription
 class Index(TemplateView):
     template_name = 'amcweb/index.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
 
 class Appointment(FormView):
     template_name = 'amcweb/appointment.html'
@@ -21,7 +17,6 @@ class Appointment(FormView):
     success_url = '/appointment'
 
     def form_valid(self, form):
-
         print(form.cleaned_data)
         form.send_email()
         return super().form_valid(form)
@@ -29,7 +24,3 @@ class Appointment(FormView):
 
 class About(TemplateView):
     template_name = 'amcweb/about.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
