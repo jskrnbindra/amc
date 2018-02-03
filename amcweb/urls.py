962ff26fb54549fb5d50e1af60acb13407444bef
Bindra <jskrnbindra@gmail.com>
2018-02-03 10:29:30 +0530

    making templates fit it

--- amcweb/urls.py
+++ amcweb/urls.py
@@ -6,5 +6,4 @@ from . import views
 app_name = 'amcweb'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('reciever/<int:pat_id>', views.reciever, name='reciever')
 ]
--- amcweb/views.py
+++ amcweb/views.py
@@ -9,15 +9,5 @@ from .models import Appointment, Patient, Prescription
 
 
 def index(request):
-    patients = Patient.objects.all()
-    context = {
-        'patients': patients
-    }
+    return render(request, 'amcweb/index.html', {})
 
-    return render(request, 'amcweb/index.html', context)
-
-
-def reciever(request, pat_id):
-    print(f'Received patient id: {pat_id}')
-
-    return HttpResponse('Done')
