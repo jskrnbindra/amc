9e6bbef3848a3a0a94fd97833fc0c795031100ed
Bindra <jskrnbindra@gmail.com>
2018-02-04 10:23:53 +0530

    bugfix: using field names instead of indices in drop down lists

--- amcweb/forms.py
+++ amcweb/forms.py
@@ -9,10 +9,10 @@ class AppointmentForm(forms.Form):
     name = forms.CharField(max_length=50)
     contact = forms.CharField(max_length=10)
     email = forms.EmailField(required=False)
-    gender = forms.ChoiceField(choices=[(-1, '--'), (0, 'Male'), (1, 'Female'), (2, 'Rather not mention')])
+    gender = forms.ChoiceField(choices=[(-1, '--'), ('Male', 'Male'), ('Female', 'Female'), ('Rather not mention', 'Rather not mention')])
     birth_year = forms.ChoiceField(choices=[(-1, '--')] + [x for x in list(map(lambda x: (x,x) ,reversed(range(1901, date.today().year + 1))))])
     visiting_on = forms.DateTimeField(input_formats=['%m/%d/%Y %H:%M'])
-    purpose = forms.ChoiceField(choices=[(-1, '--')] + [(i + 1, x) for i, x in enumerate(APPOINTMENT_TYPES)])
+    purpose = forms.ChoiceField(choices=[(-1, '--')] + [(x, x) for i, x in enumerate(APPOINTMENT_TYPES)])
     comment = forms.CharField(required=False, max_length=500, widget=forms.Textarea)
 
     def send_email(self):
