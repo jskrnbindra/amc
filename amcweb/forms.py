097bcda7f2b6716b8aa3f3ca9ce5b7371e86b2f6
Jaskaran Bindra <jskrnbindra@gmail.com>
2018-03-04 19:03:58 +0530

    Bugfix: fall back to old emoji for universal supportˆ

--- amcweb/forms.py
+++ amcweb/forms.py
@@ -11,7 +11,7 @@ def required_msg(required_suffix):
     return f'You forgot to enter {required_suffix}.🤷‍'
 
 def invalid_msg(invalid_suffix):
-    return f'That does not looks like a valid {invalid_suffix}.🧐'
+    return f'That does not looks like a valid {invalid_suffix}.🤔'
 
 def get_errors(invalid_suffix, required_suffix):
     return {
